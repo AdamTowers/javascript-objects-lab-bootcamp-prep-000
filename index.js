24c560a32a29d346dac870926aa1439f0654c5c5
Adam Towers <hello@adamtowers.design>
2018-02-12 14:32:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  //object[key] = value;
-  //return object;
   return Object.assign({}, object, { [key]: value });
 }
-
-//return Object.assign({}, obj, { [key]: value }) ??
-
-//function updateObjectWithKeyAndValue(object, key, value) {
-  //return Object.assign({}, targetObject, updatesObeject);
-//}
